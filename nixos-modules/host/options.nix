1d6721cbb85aa3094bacfe27c2bbfa4df2c41d01
Sandro <sandro.jaeckel@gmail.com>
2024-10-19 17:34:36 +0200

    Fix tapScript doc rendering

--- nixos-modules/host/options.nix
+++ nixos-modules/host/options.nix
@@ -37,9 +37,8 @@
         ''${pkgs.iproute2}/bin/ip link set "$id" master br0 up
       '';
       type = types.lines;
-      default = ''
-        ${pkgs.iproute2}/bin/ip link set "$id" up
-      '';
+      default = "${pkgs.iproute2}/bin/ip link set "$id" up";
+      defaulText = "''${pkgs.iproute2}/bin/ip link set "$id" up";
     };
 
     vms = mkOption {
